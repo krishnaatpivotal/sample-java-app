b8d863f8f2c62745e8d2f8c855f990af9451a4ad
krishnaatpivotal <kgogineni@pivotal.io>
2020-01-27 16:16:02 -0500

    test

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello owrld Yatin!!");
 
 		SpringApplication.run(SampleApplication.class, args);
 	}
